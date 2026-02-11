043f737ad36d01f1ff58dac31268257a84f24916
salman9653 <salman9915189734@gmail.com>
2026-02-11 14:18:11 +0530

    add page navagation bar for resume window with prev and next page buttons and functionality

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -2,6 +2,7 @@ import { WindowControls } from "@components";
 import WindowWrapper from "@hoc/WindowWrapper";
 import { ChevronLeft, ChevronRight, Download } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
+import { useState } from "react";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
@@ -11,6 +12,17 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const Resume = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => (numPages ? Math.min(prev + 1, numPages) : prev));
+  };
+
   return (
     <>
       <div id="window-header">
@@ -18,10 +30,6 @@ const Resume = () => {
 
         <h2>Resume.pdf</h2>
 
-        <div className="flex items-center gap-1 ml-5">
-          <ChevronLeft className="icon" />
-          <ChevronRight className="icon" />
-        </div>
         <a
           href="files/resume.pdf"
           download
@@ -31,8 +39,25 @@ const Resume = () => {
           <Download className="icon" />
         </a>
       </div>
-      <Document file="files/resume.pdf">
-        <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+      <div className="flex items-center justify-evenly bg-gray-200/50 shadow-md p-1 font-semibold">
+        <ChevronLeft
+          className="icon bg-gray-200 border border-gray-300 cursor-pointer"
+          onClick={handlePreviousPage}
+        />
+        <p className="text-xs">
+          Page {currentPage} of {numPages || "?"}
+        </p>
+
+        <ChevronRight
+          className="icon bg-gray-200 border border-gray-300 cursor-pointer"
+          onClick={handleNextPage}
+        />
+      </div>
+      <Document
+        file="files/resume.pdf"
+        onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+      >
+        <Page pageNumber={currentPage} renderTextLayer renderAnnotationLayer />
       </Document>
     </>
   );
